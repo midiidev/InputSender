9494ee93282558a7fe78b93975075cc226c840b3
Ash <101067949+Fire-Ash@users.noreply.github.com>
2022-11-12 22:43:07 -0500

    remove unused components from frontend

--- Frontend/src/components/Btn.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <button
-        class="transition ease-in-out p-3 rounded-lg bg-teal-500 hover:bg-teal-600 active:bg-teal-700 duration-300 active:scale-90"
-    >
-        <slot />
-    </button>
-</template>
